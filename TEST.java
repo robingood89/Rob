20f620e73eb804974403b30e65bea246519f1848
robingood89 <robingood89@gmail.com>
2019-11-05 22:49:38 +0300

    Вторая версия

---
+++ TEST.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argc) throws Exception {
+        Scanner scaner = new Scanner(System.in);
+        String s = "";
+        while (!s.equals("42")) {
+            s = scaner.nextLine();
+            if (!s.equals("42")) {
+                System.out.println(s);
+            }
+        }
+    }
+}
